bc97709628704673417f1a17affbf0643d7e635d
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-02-26 22:54:17 +0100

    index.js: add like and remove buttons

--- scripts/index.js
+++ scripts/index.js
@@ -39,12 +39,12 @@ function handleFormSubmit(evt) {
 editProfileButton.addEventListener('click', handleNewUserFormOpen);
 closeNewUserPopupButton.addEventListener('click', handleNewUserFormClose);
 formNewUser.addEventListener('submit', handleFormSubmit);
-
+// ---------------------------------------------------------------------------
 
 // ADD PHOTOS TO GALLERY
 const formNewItem = document.querySelector('.popup__form_new_item');
 const popupNewItem = document.querySelector('.popup_new_item');
-const addNewItemButton = document.querySelector('.profile__add-btn');
+const openNewItemButton = document.querySelector('.profile__add-btn');
 const closeNewItemPopupButton = document.querySelector('.popup__close-btn_new_item');
 // NEW ITEM POPUP FORM OPEN
 function handleNewItemFormOpen(evt) {
@@ -56,8 +56,9 @@ function handleNewItemFormClose(evt) {
 };
 
 // POPUP NEW ITEM FORM EVENTS
-addNewItemButton.addEventListener('click', handleNewItemFormOpen);
+openNewItemButton.addEventListener('click', handleNewItemFormOpen);
 closeNewItemPopupButton.addEventListener('click', handleNewItemFormClose);
+
 // GALERY PLACES
 const initialCards = [
     {
@@ -85,21 +86,75 @@ const initialCards = [
         link: 'https://images.unsplash.com/photo-1643281237857-5f14c2b9f3ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
     },
 ];
+// ADD NEW IMAGES TO GALERY
+// const popupInputImageName = document.querySelector('.popup__input_image_name');
+// const popupInputImageLink = document.querySelector('.popup__input_image_link');
+
+// const handleFormNewItemSubmit = (event) => {
+//     event.preventDefault();
+//     handleFormClose(popupNewItem);
+//     const newImage = {name: popupInputImageName.value, link: popupInputImageLink.value};
+//     initialCards.unshift(newImage);
+//     placesContainer.append(newImage);
+// };
+// formNewItem.addEventListener('submit', handleFormNewItemSubmit);
+// -----------------------------------------------------------------------------
 const placesContainer = document.querySelector('.places');
 
+// CREATE NEW CARDS
 function createNewCard(newName, newLink) {
     const newCard = document.querySelector('#cardTemplate').content.cloneNode(true);
     const cardElementHeading = newCard.querySelector('.card__title');
     const cardElementImage = newCard.querySelector('.card__image');
-    
+
     cardElementHeading.textContent = newName;
     cardElementImage.setAttribute('src', newLink);
+// ADD LIKE ONTO IMAGES
+    const imageLikeButton = newCard.querySelector('.card__like-btn');
+    const puttLike = () => {
+        imageLikeButton.classList.toggle('card__like-btn_active');
+    };
+    imageLikeButton.addEventListener('click', puttLike);
+// REMOVE CARDS
+    const removeImageButton = newCard.querySelector('.card__remove-btn');
+    const removeImage = () => {
+        const cardContainer = removeImageButton.closest('.card');
+        cardContainer.remove();
+    };
+    removeImageButton.addEventListener('click', removeImage);
+
+    cardElementImage.style.cursor = 'pointer';
+// IMAGES POPUP
+
+    const imagePopupOpen = () => {
+        const popupImageElement = document.querySelector('.popup-img');
+        handleFormOpen(popupImageElement);
+        popupImageElement.style.display = 'block';
+    }
+    cardElementImage.addEventListener('click', imagePopupOpen);
+
+
 
     return newCard;
 };
-
+// ADD CARDS INTO HTML
 initialCards.forEach(card => {
     placesContainer.append(createNewCard(card.name, card.link));
+    const popupInputImageName = document.querySelector('.popup__input_image_name');
+    const popupInputImageLink = document.querySelector('.popup__input_image_link');
+
+
+
 });
+
+
+// const handleFormNewItemSubmit = (event) => {
+//     event.preventDefault();
+//     handleFormClose(popupNewItem);
+//     const newImage = {name: popupInputImageName.value, link: popupInputImageLink.value};
+//     initialCards.unshift(newImage);
+//     placesContainer.append(newImage);
+// };
+// formNewItem.addEventListener('submit', handleFormNewItemSubmit);
 
 
